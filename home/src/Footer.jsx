c3ffeeaad5b993dbc84504f5c62ad3cca02b5cb3
andrei-justin <andreijustinalbucar@gmail.com>
2024-11-29 08:18:59 +0800

    Changed footer layout

--- home/src/Footer.jsx
+++ home/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./index.scss";
 export default function Footer() {
       return (
-            <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+            <div className="p-5 bg-blue-500 text-white text-3xl font-bold fixed bottom-0 left-0 w-full">
                   Only the Best Spinner
             </div>
       );
